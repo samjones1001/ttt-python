25358b7fd058681068ae0d6a131ec1c3c119c977
Sam Jones <sam.jones494@googlemail.com>
2019-09-25 20:27:15 +0100

    interface can print the board

--- tests/test_interface.py
+++ tests/test_interface.py
@@ -7,11 +7,36 @@ def empty_board_output():
     return '   |   |   \n-----------\n   |   |   \n-----------\n   |   |   \n'
 
 
+@pytest.fixture
+def part_filled_board_output():
+    return ' x |   |   \n-----------\n   |   |   \n-----------\n   |   |   \n'
+
+
+@pytest.fixture
+def filled_board_output():
+    return ' x | x | x \n-----------\n x | x | x \n-----------\n x | x | x \n'
+
+
 def test_prints_an_empty_grid_correctly(empty_board_output, capfd):
     empty_board_state = ['-', '-', '-', '-', '-', '-', '-', '-', '-']
     interface.render_board(empty_board_state)
     out, err = capfd.readouterr()
     assert out == empty_board_output
+
+
+def test_prints_a_part_filled_grid_correctly(part_filled_board_output, capfd):
+    part_filled_board_state = ['x', '-', '-', '-', '-', '-', '-', '-', '-']
+    interface.render_board(part_filled_board_state)
+    out, err = capfd.readouterr()
+    assert out == part_filled_board_output
+
+
+def test_prints_a_fully_filled_grid(filled_board_output, capfd):
+    filled_board_state = ['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x']
+    interface.render_board(filled_board_state)
+    out, err = capfd.readouterr()
+    assert out == filled_board_output
+
 
 
 
--- ttt/interface.py
+++ ttt/interface.py
@@ -1,2 +1,44 @@
+import math
+
+
 def render_board(board_state):
-    print(   '   |   |   \n-----------\n   |   |   \n-----------\n   |   |   ')
+    print(_build_board_output(board_state))
+
+
+def _build_board_output(board_state):
+    spaces_per_line = _find_board_size(board_state)
+    lines = _build_lines(board_state, spaces_per_line)
+    dividers = _build_dividers(spaces_per_line)
+    return dividers.join(lines)
+
+
+def _build_lines(board_state, spaces_per_line):
+    line_arrays = _split_board_to_lines(board_state, spaces_per_line)
+    return [_build_line_string(line) for line in line_arrays]
+
+
+def _find_board_size(board_state):
+    return int(math.sqrt(len(board_state)))
+
+
+def _split_board_to_lines(board_state, spaces_per_line):
+    for i in range(0, len(board_state), spaces_per_line):
+        yield board_state[i:i + spaces_per_line]
+
+
+def _build_line_string(line_array):
+    line = ' | '.join(line_array).replace('-', ' ')
+    return _pad_string(line)
+
+
+def _pad_string(string):
+    return f" {string} "
+
+
+def _build_dividers(grid_width):
+    divider = '-' * _divider_length(grid_width)
+    return f"\n{divider}\n"
+
+
+def _divider_length(grid_width):
+    return (grid_width * 3) + (grid_width - 1)
