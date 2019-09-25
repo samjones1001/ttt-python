b0c5f25389332e0ce870e7df26d9d2684960a31b
Sam Jones <sam.jones494@googlemail.com>
2019-09-25 20:42:36 +0100

    refactor test_interface.py

--- tests/test_interface.py
+++ tests/test_interface.py
@@ -1,6 +1,19 @@
 import pytest
 import ttt.interface as interface
 
+class Runner():
+    def __init__(self, capfd):
+        self._capfd = capfd
+
+    def render_board(self, board_state):
+        interface.render_board(board_state)
+        out, err = self._capfd.readouterr()
+
+        if err:
+            print(err)
+        else:
+            return out
+
 
 @pytest.fixture
 def empty_board_output():
@@ -17,25 +30,24 @@ def filled_board_output():
     return ' x | x | x \n-----------\n x | x | x \n-----------\n x | x | x \n'
 
 
-def test_prints_an_empty_grid_correctly(empty_board_output, capfd):
+@pytest.fixture
+def runner(capfd):
+    return Runner(capfd)
+
+
+def test_prints_an_empty_grid_correctly(empty_board_output, runner):
     empty_board_state = ['-', '-', '-', '-', '-', '-', '-', '-', '-']
-    interface.render_board(empty_board_state)
-    out, err = capfd.readouterr()
-    assert out == empty_board_output
+    assert runner.render_board(empty_board_state) == empty_board_output
 
 
-def test_prints_a_part_filled_grid_correctly(part_filled_board_output, capfd):
+def test_prints_a_part_filled_grid_correctly(part_filled_board_output, runner):
     part_filled_board_state = ['x', '-', '-', '-', '-', '-', '-', '-', '-']
-    interface.render_board(part_filled_board_state)
-    out, err = capfd.readouterr()
-    assert out == part_filled_board_output
+    assert runner.render_board(part_filled_board_state) == part_filled_board_output
 
 
-def test_prints_a_fully_filled_grid(filled_board_output, capfd):
+def test_prints_a_fully_filled_grid(filled_board_output, runner):
     filled_board_state = ['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x']
-    interface.render_board(filled_board_state)
-    out, err = capfd.readouterr()
-    assert out == filled_board_output
+    assert runner.render_board(filled_board_state) == filled_board_output
 
 
 
